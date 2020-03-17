f1aef1305a77f86e32537fe184159c392db130bc
Romadhon Akbar Sholeh <romadhon28akbar@gmail.com>
2020-03-17 15:48:18 +0700

    blog next js deploy

---
+++ _next/static/webpack/static/development/pages/auth/login.js.5862efaa42fec4fbb3a9.hot-update.js
@@ -0,0 +1,68 @@
+webpackHotUpdate("static/development/pages/auth/login.js",{
+
+/***/ "./pages/auth/login.js":
+/*!*****************************!*\
+  !*** ./pages/auth/login.js ***!
+  \*****************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var next_head__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! next/head */ "./node_modules/next/dist/next-server/lib/head.js");
+/* harmony import */ var next_head__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(next_head__WEBPACK_IMPORTED_MODULE_1__);
+var _jsxFileName = "/Users/macos/Sites/next-app/pages/auth/login.js";
+
+var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
+
+
+var Login = function Login() {
+  return __jsx("div", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 3
+    },
+    __self: this
+  }, __jsx(next_head__WEBPACK_IMPORTED_MODULE_1___default.a, {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 4
+    },
+    __self: this
+  }, __jsx("title", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 5
+    },
+    __self: this
+  }, "Login"), __jsx("link", {
+    rel: "icon",
+    href: "/favicon.ico",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 6
+    },
+    __self: this
+  })), __jsx("main", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 9
+    },
+    __self: this
+  }, __jsx("h1", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 10
+    },
+    __self: this
+  }, "Login Yuk")));
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (Login);
+
+/***/ })
+
+})
+//# sourceMappingURL=login.js.5862efaa42fec4fbb3a9.hot-update.js.map
